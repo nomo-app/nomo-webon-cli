b01dc139a97e5c44f5ada3cd938e2fe1a37908f9
dev-ig <dev2@nomo.app>
2023-11-20 15:08:40 +0100

    Added new command init functionality:  create nomo_cli.config.js

--- src/init/interface.ts
+++ src/init/interface.ts
@@ -1,38 +1,61 @@
 export interface NomoManifest {
-    /**
-     * If min_nomo_version is set, then outdated versions of the Nomo App will refuse to install the WebOn.
-     */
-    min_nomo_version?: string | null;
-    /**
-     * nomo_manifest_version should be 1.1.0.
-     */
-    nomo_manifest_version: string;
-    /**
-     * A list of permissions for security-critical features.
-     */
-    permissions: string[];
-    /**
-     * webon_id should be the reverse-domain of a domain that is owned by the WebOn-author.
-     * See https://en.wikipedia.org/wiki/Reverse_domain_name_notation for more details about the reverse domain name notation.
-     */
-    webon_id: string;
-    /**
-     * webon_name is the user-visible name of the WebOn.
-     */
-    webon_name: string;
-    /**
-     * webon_version should comply with the semantic versioning standard.
-     * See https://semver.org/ for details.
-     */
-    webon_version: string;
-    /**
-     * If true, then the WebOn could be displayed in both card-mode and fullscreen-mode.
-     * If false, then the WebOn will only be displayed in fullscreen-mode.
-     */
-    card_mode?: boolean;
-    /**
-     * If defined, then the WebOn can decide whether a navigation bar should be shown or not.
-     */
-    show_navbar?: boolean;
-  }
-  
+  /**
+   * If min_nomo_version is set, then outdated versions of the Nomo App will refuse to install the WebOn.
+   */
+  min_nomo_version?: string | null;
+  /**
+   * nomo_manifest_version should be 1.1.0.
+   */
+  nomo_manifest_version: string;
+  /**
+   * A list of permissions for security-critical features.
+   */
+  permissions: string[];
+  /**
+   * webon_id should be the reverse-domain of a domain that is owned by the WebOn-author.
+   * See https://en.wikipedia.org/wiki/Reverse_domain_name_notation for more details about the reverse domain name notation.
+   */
+  webon_id: string;
+  /**
+   * webon_name is the user-visible name of the WebOn.
+   */
+  webon_name: string;
+  /**
+   * webon_version should comply with the semantic versioning standard.
+   * See https://semver.org/ for details.
+   */
+  webon_version: string;
+  /**
+   * If true, then the WebOn could be displayed in both card-mode and fullscreen-mode.
+   * If false, then the WebOn will only be displayed in fullscreen-mode.
+   */
+  card_mode?: boolean;
+  /**
+   * If defined, then the WebOn can decide whether a navigation bar should be shown or not.
+   */
+  show_navbar?: boolean;
+}
+
+export interface NomoCliConfig {
+  deployTargets: {
+    production: {
+      rawSSH: {
+        sshHost: string;
+        sshBaseDir: string;
+        publicBaseUrl: string;
+      };
+    };
+    staging: {
+      rawSSH: {
+        sshHost: string;
+        sshBaseDir: string;
+        publicBaseUrl: string;
+        sshPort: number;
+      };
+    };
+  };
+}
+export interface GeneratedFile {
+  filePath: string;
+  content: string;
+}
