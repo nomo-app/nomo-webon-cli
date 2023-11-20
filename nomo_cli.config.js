7b0c35422d01d36785d42a82a044afea3bfae6cb
dev-ig <dev2@nomo.app>
2023-11-20 14:17:14 +0100

    Removed comments from nomo_cli.config.js

--- nomo_cli.config.js
+++ nomo_cli.config.js
@@ -6,41 +6,18 @@ const nomoCliConfig = {
   deployTargets: {
     production: {
       rawSSH: {
-        /**
-         * sshHost is the remote-host for deploying your WebOn.
-         * Please ensure that you have valid SSH-credentials before trying to deploy anything.
-         */
         sshHost: "root@<IP-address>",
-
-        /**
-         * sshBaseDir is a remote-directory for deploying your WebOn.
-         */
         sshBaseDir: "/var/www/production_webons/${webon_id}/",
-
-        /**
-         * publicBaseUrl is a URL where sshBaseDir gets exposed to the Internet.
-         * publicBaseUrl is needed to generate a deeplink for installing your WebOn.
-         * For example, you could configure an nginx-server to map sshBaseDir to a publicBaseUrl.
-         */
-        publicBaseUrl: "https://w.nomo.app",
+        publicBaseUrl: "https://w.nomo.app/${webon_id}",
       },
     },
     staging: {
       rawSSH: {
-        /**
-         * sshHost could be taken from an environment-variable to hide your target IP address.
-         */
         sshHost: process.env.SSH_TARGET,
         sshBaseDir: "/var/www/html/webons/${webon_id}/",
-        publicBaseUrl: "https://staging.nomo.app",
-
-        /**
-         * Optional. The default sshPort is 22.
-         */
+        publicBaseUrl: "https://staging.nomo.app/${webon_id}",
         sshPort: 51110,
       },
-      // ZENCON deployments are not yet available. Please use rawSSH for the time being.
-      //zencon: {},
     },
   },
 };
@@ -48,5 +25,3 @@ const nomoCliConfig = {
 module.exports = {
   nomoCliConfig,
 };
-
-//console.log("nomoCliConfig", JSON.stringify(nomoCliConfig, null, 2));
