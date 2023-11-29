6546b3596b4eea3a3d293973324b05b255296b7a
dev-ig <dev2@nomo.app>
2023-11-29 15:33:26 +0100

    Added first unit test

--- test/test-util/test-util.ts
+++ test/test-util/test-util.ts
@@ -2,8 +2,11 @@ import { exec } from "child_process";
 import { join } from "path";
 
 export function buildCliCommand(args: string) {
-  const cliCommand = process.platform === "win32" ? "nomo-webon-cli" : join(process.cwd(), "bin", "nomo-webon-cli");
-  
+  const cliCommand =
+    process.platform === "win32"
+      ? "nomo-webon-cli"
+      : join(process.cwd(), "bin", "nomo-webon-cli");
+
   return `${process.platform === "win32" ? "npx " : ""}${cliCommand} ${args}`;
 }
 
@@ -49,7 +52,7 @@ function runCommandExpectFailure(
   console.log(`Run expect-fail-command \'${cmd}\'`);
 
   return new Promise((resolve, reject) => {
-    exec(cmd,{env: env}, (error, stdout, stderr) => {
+    exec(cmd, { env: env }, (error, stdout, stderr) => {
       console.log(stdout);
       console.log(stderr);
       if (error && error.code !== 0) {
@@ -65,5 +68,56 @@ function runCommandExpectFailure(
 }
 
 function buildFinalCommand(cmd: string, pwd?: string) {
-    return cmd;
+  return cmd;
+}
+
+export async function runUnitTestExpectFailure(
+  args: string,
+  pwd?: string
+): Promise<string> {
+  const cmd = buildCliCommand(args);
+  return await runCommandAndCaptureError(cmd, pwd);
+}
+
+async function runCommandAndCaptureError(
+  cmd: string,
+  pwd?: string,
+  env?: Record<string, string | undefined>
+): Promise<string> {
+  cmd = buildFinalCommand(cmd, pwd);
+  console.log(`Run command \'${cmd}\'`);
+
+  try {
+    const { stdout, stderr } = await runCommandAsync(cmd, { env });
+    console.log(stdout);
+
+    if (stderr) {
+      console.error(stderr);
+      return stderr;
+    }
+
+    console.error(`Command \'${cmd}\' succeeded unexpectedly.`);
+    return ""; // Return an empty string or any other indicator if needed.
+  } catch (error: any) {
+    console.error(error);
+    return error.message || "Unknown error occurred.";
+  }
+}
+
+function runCommandAsync(
+  cmd: string,
+  options?: Record<string, any>
+): Promise<{ stdout: string; stderr: string }> {
+  return new Promise((resolve, reject) => {
+    exec(cmd, options, (error, stdout, stderr) => {
+      const stdoutString = stdout.toString();
+      const stderrString = stderr.toString();
+
+      if (error) {
+        reject({ stdout: stdoutString, stderr: stderrString });
+      } else {
+        resolve({ stdout: stdoutString, stderr: stderrString });
+      }
+    });
+  });
 }
--- test/unitTest/build-tar-gz/build-tar.test.ts
+++ test/unitTest/build-tar-gz/build-tar.test.ts
@@ -1,9 +1,13 @@
-import { runE2ETestExpectFailure } from "../../test-util/test-util";
-import { getDebugPath } from "../../../src/util/util";
+import { runUnitTestExpectFailure } from "../../test-util/test-util";
+import { enableUnitTest } from "../../../src/util/util";
 
-test("assetDir not a dir", async () => {
-  const output = await runE2ETestExpectFailure("build README.md");
-  expect(output).toBe(
-    `ERROR: ${getDebugPath("README.md")} is not a directory.\n`
+enableUnitTest();
+
+test("missing required file", async () => {
+  const output = await runUnitTestExpectFailure(
+    "build test_assets/out_incomplete/out"
+  );
+  expect(output).toMatch(
+    /Error: The 'out' directory is missing the following required files: .* Use nomo-webon-cli init if you do not have a nomo_manifest.json yet.\n/
   );
 });
